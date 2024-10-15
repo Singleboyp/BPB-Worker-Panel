9ede8f077f2d98714c84e8335799b904ebb4be90
Singleboyp <singleboyp@gmail.com>
2024-10-15 10:11:18 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4085,7 +4085,7 @@ var SignJWT = class extends ProduceJWT {
 };
 
 // src/worker.js
-var userID = "89b3cbba-e6ac-485a-9481-976a0415eab9";
+var userID = "cc66ef32-c3ab-4276-8b9a-99796145b078";
 var trojanPassword = `bpb-trojan`;
 var proxyIPs = ["bpb.yousef.isegaro.com"];
 var defaultHttpPorts = ["80", "8080", "2052", "2082", "2086", "2095", "8880"];
